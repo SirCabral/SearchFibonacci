e360fa61eb2c37279e316e145273d3ecb00bf805
Renan Cabral <renancabralfotografia@gmail.com>
2022-06-21 15:55:58 -0300

    Search Fibonacci 1.0

---
+++ src/Program.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+		
+		
+		System.out.println("Insira um numero para verificar se pertence ao Fibonacci: ");
+		int numberToSearch = sc.nextInt();
+			if (searchFibonacci(numberToSearch)) {
+				System.out.println(numberToSearch + " pertence ao Fibonacci");
+			}
+			else {
+				System.out.println(numberToSearch + " não pertence ao Fibonacci");
+			}
+		
+		sc.close();
+	    }
+	
+	//procura o numero informado no calculo fibonacci e retorna um boolean
+	public static boolean searchFibonacci(Integer number) {
+        int num1 = 1, num2 = 0;        
+        for(int i = 0; i < 100; i++){
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            if (num1 == number) {
+            	return true;           	
+            }                 
+        }
+        return false;  
+	}
+
+}
